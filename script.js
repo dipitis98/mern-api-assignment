e925707074deb815727d8beb658aeaf7e8949d05
Dip Paul <dip1221paul@gmail.com>
2025-12-27 23:44:54 +0600

    js edited

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 // loader
-document.addEventListener("load", () => {
-    const loader = document.querySelector(".loader")
-    loader.classList.add("loader-hidden")
-
-    const loaderParent = document.getElementsByClassName("recipe-container")
-
-    loader.addEventListener("transitionend", () => {
-        document.loaderParent.removeChild("loader")
-    })
-})
+const loader = document.querySelector(".loader")
+function showLoader() {
+    loader.classList.remove
+}
 
 // api call
+const grid = document.getElementById("recipe-grid");
 async function getFoods() {
     const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-    const foodData = await axios.get(apiUrl)
-    const food = foodData.data.meals[0].strMealThumb
-    console.log(food)
-    document.getElementById("box").innerHTML = food
+    const response = await axios.get(apiUrl)
+    const foodList = response.data.meals
+    console.log(foodList)
+    for (const meal of foodList) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <div class="card-body">
+              <h3>${meal.strMeal}</h3>
+              <p>${meal.strInstructions.substring(0, 100)}...</p>
+              <button class="view-btn" onclick="openModal('${
+                meal.idMeal
+              }')">VIEW DETAILS</button>
+            </div>
+          `;
+    grid.appendChild(card);
+    }
 }
 getFoods()
